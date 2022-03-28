8dcce5078f7134125554267d1ac1aab4c78544a7
hangoh <gohyuhan12345@gmail.com>
2022-03-28 22:59:15 +0900

    first commit

--- Magic_ball/ViewController.swift
+++ Magic_ball/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImageview: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButtonPressed(_ sender: Any) {
+        let imageArray = ["ball1","ball2","ball3","ball4","ball5"]
+        
+        ballImageview.image = UIImage(named: imageArray[Int.random(in: 0...4)])
+    }
+    
 }
 
